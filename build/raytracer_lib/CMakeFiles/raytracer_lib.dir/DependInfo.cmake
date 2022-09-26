8e8b77eaabf0d0f9801c3eab51032a9ec37498bf
Daniel14gc <gon20293@uvg.edu.gt>
2022-09-25 21:54:17 -0600

    feat: snowman

---
+++ build/raytracer_lib/CMakeFiles/raytracer_lib.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/daniel14gc/Graficas/raytracer_cpp/raytracer_lib/ray.cpp" "raytracer_lib/CMakeFiles/raytracer_lib.dir/ray.cpp.o" "gcc" "raytracer_lib/CMakeFiles/raytracer_lib.dir/ray.cpp.o.d"
+  "/home/daniel14gc/Graficas/raytracer_cpp/raytracer_lib/sphere.cpp" "raytracer_lib/CMakeFiles/raytracer_lib.dir/sphere.cpp.o" "gcc" "raytracer_lib/CMakeFiles/raytracer_lib.dir/sphere.cpp.o.d"
+  "/home/daniel14gc/Graficas/raytracer_cpp/raytracer_lib/vector3.cpp" "raytracer_lib/CMakeFiles/raytracer_lib.dir/vector3.cpp.o" "gcc" "raytracer_lib/CMakeFiles/raytracer_lib.dir/vector3.cpp.o.d"
+  "/home/daniel14gc/Graficas/raytracer_cpp/raytracer_lib/writer.cpp" "raytracer_lib/CMakeFiles/raytracer_lib.dir/writer.cpp.o" "gcc" "raytracer_lib/CMakeFiles/raytracer_lib.dir/writer.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/raytracer_lib/CMakeFiles/raytracer_lib.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytracer_lib.dir/ray.cpp.o"
+  "CMakeFiles/raytracer_lib.dir/ray.cpp.o.d"
+  "CMakeFiles/raytracer_lib.dir/sphere.cpp.o"
+  "CMakeFiles/raytracer_lib.dir/sphere.cpp.o.d"
+  "CMakeFiles/raytracer_lib.dir/vector3.cpp.o"
+  "CMakeFiles/raytracer_lib.dir/vector3.cpp.o.d"
+  "CMakeFiles/raytracer_lib.dir/writer.cpp.o"
+  "CMakeFiles/raytracer_lib.dir/writer.cpp.o.d"
+  "libraytracer_lib.pdb"
+  "libraytracer_lib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracer_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
